26c299c11fdcf0e222181ad8d6b811f155f049d9
Lucas <lucasluvn@gmail.com>
2026-05-12 14:41:56 -0300

    Finalizando o HTML E O CSS

---
+++ script.js
@@ -0,0 +1,13 @@
+// PLANEJAMENTO DO PROJETO PETSHOP
+// 1. Criar função para validar nome do pet
+// 2. Criar lógica de seleção de serviço (Banho, Tosa, Consulta)
+// 3. Adicionar evento de clique no botão de agendar
+// 4. Salvar os dados no LocalStorage
+// 5. Mostrar mensagem de confirmação para o usuário
+
+
+
+const schedulesList = document.querySelector(".schedule-list");
+schedulesList.addEventListener("click",() =>{
+  alert("button was clicked");
+});
