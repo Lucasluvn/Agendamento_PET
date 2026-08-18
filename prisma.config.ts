771be2089282f5e69ccfdaa1780f1123b1859f76
Lucas <lucasluvn@gmail.com>
2026-08-17 21:40:09 -0300

    Criando os arquivos prisma e configurando o Prisma

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+// diz pro Prisma onde encontrar o arquivo schema.prisma e também guarda a conexão com o banco de dados (a URL que vem do .env) — que antes ficava dentro do próprio schema.prisma
+
+import 'dotenv/config'
+import { defineConfig } from 'prisma/config'
+
+
+export default defineConfig({
+  schema: 'server/prisma/schema.prisma',
+  datasource:{
+    url: process.env.DATABASE_URL,
+  }
+})
